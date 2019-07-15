bf63c29edab5e2692525ca32324adf101f518319
lpl <mohamed.ali.saidina+github@gmail.com>
2019-07-15 14:17:56 +0200

    Add review for backend netapi

--- alcali/web/backend/netapi.py
+++ alcali/web/backend/netapi.py
@@ -5,6 +5,12 @@ from urllib.error import URLError
 
 import pendulum
 from pepper import Pepper, PepperException
+# TODO: Clear the dependency between this module and the middleware:
+#       There is 3 functions in this module, that can be consider part
+#       of the business logic, that directly depend on that
+#       django middleware, to to test the business logic
+#       we'll need the django context. It would be nice
+#       to isolate the two, by dependency injection for example.
 from django_currentuser.middleware import get_current_user
 
 from ..utils.input import RawCommand
@@ -16,16 +22,24 @@ url = os.environ.get("SALT_URL")
 @contextmanager
 def api_connect():
     user = get_current_user()
+    # NOTE: The ignore_ssl_errors value should be the choice of the user,
+    #       especially if the flag is switch from the original one.
     api = Pepper(url, ignore_ssl_errors=True)
     try:
         login_ret = api.login(
+            # XXX: Why do we have to force the type of user.username ?
+            # NOTE: I think it is better to user keyword when theire is
+            #       some sort of computation on the right, even if it is a simple
+            #       one, it make the api calls more readable.
             str(user.username), user.user_settings.token, os.environ.get("SALT_AUTH")
         )
         user.user_settings.salt_permissions = json.dumps(login_ret["perms"])
         user.save()
         yield api
 
+    # XXX: Where does ConnectionRefusedError comes from ?
     except (PepperException, ConnectionRefusedError, URLError) as e:
+        # TODO: Switch to a logger instance which the user can control on it.
         print("Can't connect to {url}: {e}".format(url=url, e=e))
         yield
 
@@ -39,8 +53,18 @@ def get_keys(refresh=False):
             "minions_pre": "unaccepted",
             "minions": "accepted",
         }
-
+        # NOTE: In this function(and all other in this module),
+        #       you directly depend on api_connect which is performing both
+        #         - a external HTTP call
+        #         - a DB operation(user.save)
+        #       So to unit test get_keys you'll need to set up a
+        #       DB and salt server, by injecting either the api_connect
+        #       callable or better an api object, we can isolate the behavior
+        #       of each function and test them.
         with api_connect() as api:
+            # NOTE: I think overloading the same variable is unnessessary,
+            #       and can create confusion why not just
+            #       api.wheel("key.list_all")["return"][0]["data"]["return"]
             api_ret = api.wheel("key.list_all")
             api_ret = api_ret["return"][0]["data"]["return"]
 
@@ -50,6 +74,7 @@ def get_keys(refresh=False):
                     finger_ret = api.wheel(
                         "key.finger", match=minion, hash_type="sha256"
                     )
+                    # NOTE: Same remark here about overloading variables.
                     finger_ret = finger_ret["return"][0]["data"]["return"][key]
                     obj, created = Keys.objects.update_or_create(
                         minion_id=minion,
@@ -81,6 +106,20 @@ def refresh_minion(minion_id):
                 "name", "function"
             ).distinct()
             for field in minion_fields:
+                # NOTE: The S/N ratio here is bigger than necessary,
+                #       1. name is only used once, so no need to alias it
+                #       2. func is used twice but the difference in performance
+                #          between a local access and a dict key access seems
+                #          negligeable compared to the semantic value of field.
+                #       3. parsed is also unnessessary, what we really want is
+                #          custom_field_return so why not just
+                #          run_raw(comm_inst.parse())
+                #       It will remove 3 variables that add noise
+                #       to our main goal:
+                #       run a salt function on a minion,
+                #       create a custom field with the result.
+                #       Which stated like this can justify its own function
+                #       by the way.
                 name = field["name"]
                 func = field["function"]
                 comm_inst = RawCommand("salt {} {}".format(minion_id, func))
@@ -97,6 +136,7 @@ def refresh_minion(minion_id):
 def run_job(tgt, fun, args, kwargs=None):
     with api_connect() as api:
         api_ret = api.local(tgt, fun, arg=args, kwarg=kwargs)
+    # XXX: Why not just returning api_ret["return"][0]?
     api_ret = api_ret["return"][0]
     return api_ret
 
@@ -171,6 +211,8 @@ def manage_key(action, target, kwargs):
 
 
 def set_perms():
+    # XXX: Which of those instruction can throw the URLError ?
+    #      the rest can live outside of this try/except block
     try:
         user = get_current_user()
         api = Pepper(url, ignore_ssl_errors=True)
@@ -179,6 +221,8 @@ def set_perms():
         )
         user.user_settings.salt_permissions = json.dumps(login_ret["perms"])
         user.save()
+    # NOTE: I'll recommend contextlib.suppress,
+    # https://docs.python.org/3/library/contextlib.html#contextlib.suppress
     except URLError:
         pass
 
@@ -187,6 +231,13 @@ def refresh_schedules(minion=None):
     minion = minion or "*"
     with api_connect() as api:
         api_ret = api.local(minion, "schedule.list", kwarg={"return_yaml": False})
+    # XXX: If I understand correctly api_ret["return"][0] is a dict with each
+    #      key being the minion_id and each value being a dict of jobs.
+    #      So can't we use something like:
+    #      `for minion_id, minion_jobs in api_ret["return"][0].items():`
+    #      Or since we're looping over it, and then return it maybe a alias
+    #      can be usefull here: minion = api_ret["return"][0]
+    #      But that will require a change of previous `minion` to `minion_name`
     for minion_id in api_ret["return"][0]:
         # TODO: error mgmt
         minion_jobs = api_ret["return"][0][minion_id]
@@ -207,6 +258,7 @@ def manage_schedules(action, name, minion):
             if "delete" in action:
                 Schedule.objects.filter(minion=minion, name=name).delete()
             else:
+                # XXX: Why not refresh before get so you have to try it only once ?
                 try:
                     sched = Schedule.objects.filter(minion=minion, name=name).get()
                 except Schedule.DoesNotExist:
@@ -214,6 +266,7 @@ def manage_schedules(action, name, minion):
                     try:
                         sched = Schedule.objects.filter(minion=minion, name=name).get()
                     except Schedule.DoesNotExist:
+                        # XXX: If we return False here, shouldn't we return True here
                         return False
                 loaded_job = sched.loaded_job()
                 if "enable" in action:
